77ca3edde3ff2a4beb19eaf2734d41edbd5b3951
kdkbear <kbear02.ubc@gmail.com>
2026-09-22 10:00:43 -0700

    added demo analysis tab to practice using GIT

---
+++ analysis.R
@@ -0,0 +1,11 @@
+#view the first few rows of mtcars dataset
+
+head(mtcars)
+
+#calculate the ave mpg among all cars, saved as a variable
+
+mean_mpg <- mean(mtcars$mpg)
+
+#Print mean mpg
+
+mean_mpg
